568a751288ca1be91de6705bf9d05bdaa34280e7
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2024-01-12 19:52:29 +0700

    penambahan login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,49 @@
 import './App.css';
+import Login from './pages/login';
 import SideBar from './component/sidebar/Sidebar';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import KelolaAkun from './pages/kelolaAkun';
 import JadwalJumat from './pages/JadwalJumat';
 import KelolaPimpinanJemaah from './pages/KelolaPimpinanJemaah';
 import GeneratePengajian from './pages/GeneratePengajian';
 import ValidasiJumat from './pages/ValidasiJumat';
 
+
 function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <div className='AppGlass'>
-        <Router>
-        <SideBar/>
-          <Routes>
-              <Route path="/Accounts" element={<KelolaAkun />} />
-              <Route path="/JadwalJumat" element={<JadwalJumat />} />
-              <Route path="/PimpinanJemaah" element={<KelolaPimpinanJemaah />} />
-              <Route path="/jadwal/pengajian" element={<GeneratePengajian />} />
-              <Route path="/Validate/jumat" element={<ValidasiJumat />} />
-            </Routes>
-        </Router>
-      </div>
-    </div>
+  const isLoggedIn = true; 
+
+  return (    
+      <Router>
+        <Routes>
+          {/* Rute untuk halaman login */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Rute untuk halaman dashboard */}
+          <Route
+            path="/dashboard/*"
+            element={
+              isLoggedIn ? (
+                <div className="App">
+                  <div className='AppGlass'>
+                    <SideBar />
+                    <Routes>
+                      <Route path="/" element={<Navigate to="/dashboard/Accounts" />} />
+                      <Route path="/Accounts" element={<KelolaAkun />} />
+                      <Route path="/JadwalJumat" element={<JadwalJumat />} />
+                      <Route path="/PimpinanJemaah" element={<KelolaPimpinanJemaah />} />
+                      <Route path="/jadwal/pengajian" element={<GeneratePengajian />} />
+                      <Route path="/Validate/jumat" element={<ValidasiJumat />} />
+                    </Routes>
+                  </div>
+                </div>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+        </Routes>
+      </Router>
   );
 }
 
