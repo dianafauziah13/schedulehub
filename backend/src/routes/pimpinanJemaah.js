4fac3395bd1d211666ccb499f3288327345b7d1a
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-02-10 18:20:59 +0700

    update pimpinan jemaah by id

--- backend/src/routes/pimpinanJemaah.js
+++ backend/src/routes/pimpinanJemaah.js
@@ -5,6 +5,7 @@ const PJController = require('../controllers/pimpinanJemaah');
 // Menambahkan pimpinan jemaah baru
 router.post('/add', PJController.addPJ);
 router.get('/', PJController.getAllPJ);
+router.put('/update/:id', PJController.updatePJById);
 router.delete('/delete/:id', PJController.deletePJByID);
 
 module.exports = router;
