e25aaf2fec550c31b895df8d1d70ad9f0600fa27
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-03-11 10:38:23 +0700

    Perbaiki models Pimpinan Jemaah

--- backend/src/models/pimpinanJemaah.js
+++ backend/src/models/pimpinanJemaah.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
 const pimpinanjemaahSchema = new mongoose.Schema({
+    KetuaPJ: {type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'},
     mosqueName:{
         type: String,
     },
     topicOfKajian:{
         type: String,
     },
-    scopeDakwah:{
-        type: mongoose.Schema.Types.ObjectId, ref: 'ScopeDakwah',
-    }
-
+    scopeDakwahJumat:[
+        {
+            Minggu_ke: {type:Number},
+            scopeDakwah: {type: mongoose.Schema.Types.ObjectId, ref: 'ScopeDakwah'},
+        }
+    ]
 });
 
 const  PimpinanJemaah= mongoose.model('PimpinanJemaah', pimpinanjemaahSchema);
