8342365458fab4cb2867ea994eb07a2fc23b0a30
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-03-12 11:12:35 +0700

    memperbaiki kerangka penugasan, penambahan embedding

--- backend/src/models/penugasan.js
+++ backend/src/models/penugasan.js
@@ -6,8 +6,12 @@ const penugasanSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    Mubaligh:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}],
-
+    ListOfMubaligh:[
+        {
+            KhutbahJumat:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}],
+            Pengajian:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}]
+        }
+    ]
 });
 
 const  penugasan= mongoose.model('penugasan', penugasanSchema);
