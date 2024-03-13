4fb1f3796cc369a150a6a824ac7baa406ddc8acb
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-03-13 11:49:12 +0700

    sedikit penempatan

--- backend/src/models/penugasan.js
+++ backend/src/models/penugasan.js
@@ -6,8 +6,9 @@ const penugasanSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    MubalighKhutbahJumat:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}],
-    MubalighPengajian:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}]
+    KhutbahJumat:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}],
+    Pengajian:[{type: mongoose.Schema.Types.ObjectId, ref: 'mubaligh'}]
+
 });
 
 const  penugasan= mongoose.model('penugasan', penugasanSchema);
