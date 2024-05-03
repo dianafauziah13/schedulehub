78b17b020c42d92dd57bbb0aacb7e133580d5fdd
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-05-03 21:37:00 +0700

    perubahan struktur penugasan

--- backend/src/tempatPenugasan/penugasan.service.ts
+++ backend/src/tempatPenugasan/penugasan.service.ts
@@ -3,15 +3,24 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { TempatPenugasanSchema } from './schemas/penugasan.schema';
 import { TempatPenugasanSchemaDto } from './dto/create-penugasan.dto';
+import { PimpinanjemaahSchema } from 'src/pimpinanjamaah/schemas/pimpinanjamaah.schema';
+import { MubalighSchema } from 'src/mubaligh/schemas/mubaligh.schema';
 
 @Injectable()
 export class TempatPenugasanService {
   constructor(
     @InjectModel('TempatPenugasanSchema')
     private tempatPenugasanModel: Model<TempatPenugasanSchema>,
+    private mubalighModel: Model<MubalighSchema>,
+    private pimpinanjemaahModel: Model<PimpinanjemaahSchema>,
   ) {}
 
   async createTempatPenugasan(tempatPenugasanDto: TempatPenugasanSchemaDto): Promise<TempatPenugasanSchema> {
+    const pimpinanjemaah = await this.pimpinanjemaahModel.find().exec();
+    const penugasan = await this.tempatPenugasanModel.find().exec();
+    const mubaligh = await this.mubalighModel.find().exec();
+    // manipulasi data
+
     const newTempatPenugasan = new this.tempatPenugasanModel(tempatPenugasanDto);
     return await newTempatPenugasan.save();
   }
--- backend/src/tempatPenugasan/schemas/penugasan.schema.ts
+++ backend/src/tempatPenugasan/schemas/penugasan.schema.ts
@@ -3,17 +3,84 @@ import mongoose, { Document, Schema as MongooseSchema } from 'mongoose';
 import { MubalighSchema } from 'src/mubaligh/schemas/mubaligh.schema';
 import { PimpinanjemaahSchema } from 'src/pimpinanjamaah/schemas/pimpinanjamaah.schema';
 
+@Schema()
+export class scopedakwahjumat extends Document{
+  @Prop({ type: String })
+  nama: String; 
+
+  @Prop({ type: Number })
+  minggu_ke: Number;
+}
+
+@Schema()
+export class scopedakwahpengajian extends Document{
+  @Prop({ type: [{ nama: { type: String}, minimal: { type: Number } }] })
+  Keahlian: { nama: String; minimal: Number }[];
+
+  @Prop({ type: Number })
+  minggu_ke: Number;
+
+  @Prop({ type: String })
+  hari: String;
+
+  @Prop({ type: String })
+  waktu: String;
+
+  @Prop({ type: String })
+  topik_kajian: String;
+}
+
+@Schema()
+export class pimpinan extends Document {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'PimpinanjemaahSchema' })
+  _id: PimpinanjemaahSchema; 
+  @Prop({ type: String })
+  nama: String; 
+  @Prop({ type: [scopedakwahjumat] })
+  scope_dakwah_jumat: scopedakwahjumat[];
+  @Prop({ type: [scopedakwahpengajian]})
+  scope_dakwah_pengajian: scopedakwahpengajian[]
+}
+
+@Schema()
+export class mubaligh_jumat extends Document {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'MubalighSchema' })
+  _id: MubalighSchema; 
+  @Prop({ type: String })
+  nama: String; 
+  @Prop({ type: String })
+  scope_dakwah: String;
+  @Prop({ type: [Number]})
+  ketersediaan_waktu_jumat: Number[];
+  @Prop({ type: [{ nama: { type: String}, rating: { type: Number } }] })
+  Keahlian: { nama: String; rating: Number }[];
+}
+
+@Schema()
+export class mubaligh_pengajian extends Document {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'MubalighSchema' })
+  _id: MubalighSchema; 
+  @Prop({ type: String })
+  nama: String; 
+  @Prop({ type: String })
+  scope_dakwah: String;
+  @Prop({ type: [{ minggu_ke: {type: String}, hari: {type:String} }] })
+  ketersediaan_waktu_pengajian: { minggu_ke: String; hari: String }[];
+  @Prop({ type: [{ nama: { type: String}, rating: { type: Number } }] })
+  Keahlian: { nama: String; rating: Number }[];
+}
+
 
 @Schema()
 export class Penugasan extends Document {
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'PimpinanjemaahSchema'})
-  idPimpinanJemaah: PimpinanjemaahSchema; 
+  @Prop({ type: pimpinan })
+  pimpinan: pimpinan;
 
-  @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: 'MubalighSchema' })
-  Mubaligh_KhutbahJumat: [MubalighSchema]; 
+  @Prop({ type: [mubaligh_jumat] })
+  mubaligh_khutbah_jumat: mubaligh_jumat[];
 
-  @Prop({ type: [mongoose.Schema.Types.ObjectId], ref: 'MubalighSchema' })
-  Mubaligh_KhutbahPengajian: [MubalighSchema];
+  @Prop({ type: [mubaligh_pengajian] })
+  Mubaligh_Khutbah_pengajian: [mubaligh_pengajian[]];
 }
 
 @Schema()
