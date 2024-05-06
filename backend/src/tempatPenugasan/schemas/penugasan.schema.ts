1109c8c93486b54154b4bc6931601b9c00954592
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2024-05-06 12:34:43 +0700

    penambahan di jumat

--- backend/src/tempatPenugasan/schemas/penugasan.schema.ts
+++ backend/src/tempatPenugasan/schemas/penugasan.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document, Schema as MongooseSchema } from 'mongoose';
+import { PimpinanjemaahSchema } from 'src/pimpinanjamaah/schemas/pimpinanjamaah.schema';
 
 @Schema()
 export class Penugasan extends Document {
-  @Prop({ type: mongoose.Schema.Types.ObjectId })
-  idPimpinanJemaah: mongoose.Schema.Types.ObjectId; 
+  @Prop({ type: PimpinanjemaahSchema })
+  idPimpinanJemaah: PimpinanjemaahSchema; 
 
   @Prop({ type: [mongoose.Schema.Types.ObjectId] })
   Mubaligh_KhutbahJumat: [mongoose.Schema.Types.ObjectId]; 
