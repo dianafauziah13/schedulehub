5e047561d83c19f59d530675aae392a8885f4559
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2024-05-23 22:31:36 +0700

    tampilan jadwal pengajian

--- frontend/src/pages/JadwalPengajian.jsx
+++ frontend/src/pages/JadwalPengajian.jsx
@@ -5,47 +5,91 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const JadwalPengajian = () => {
     const [startDate, setStartDate] = useState(new Date());
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch("http://localhost:3000/genetarePengajian/by-date", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },    
+            body: JSON.stringify(
+                    {
+                        bulan: startDate.getMonth()+1,
+                        tahun: startDate.getFullYear()
+                    }
+                )
+            });
+            const result = await response.json();
+            console.log(result);
+
+            let tampilJadwal = [];
+            result.data.forEach(value => {
+                tampilJadwal.push({
+                    "PimpinanJemaah":value.jadwal.PimpinanJamaah,
+                    "Minggu_ke": value.jadwal.minggu_ke,
+                    "hari":value.jadwal.hari,
+                    "Mubaligh":value.jadwal.Mubaligh
+                })
+            });
+            setData(tampilJadwal);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         // <div className='bg-bg h-screen w-screen overflow-hidden'>
-            <div className='flex flex-col items-center w-[98%] ml-[80px] pt-6'>
-                <h1 className='text-[30px] font-montserrat mb-7'>Jadwal Pengajian Rutin</h1>
-                <div className='flex items-center w-[98%] pb-10'>
-                    <DatePicker
-                        className="rounded mx-auto text-center"
-                        selected={startDate}
-                        onChange={(date) => setStartDate(date)}
-                        dateFormat="MM/yyyy"
-                        showMonthYearPicker
-                    />
-                    <FaCalendarAlt className="ml-2" />
-                    </div>
-                    <div className="flex flex-col items-center w-[98%] bg-white px-5 py-3 shadow-md font-montserrat rounded-md">
-                    <div className="w-full">
-                        <table className="table-auto w-full border-separate border-spacing-y-3">
+        <div className='flex flex-col items-center w-[98%] ml-[80px] pt-6'>
+        <h1 className='text-[30px] font-montserrat mb-7'>Jadwal Pengajian Rutin</h1>
+        <div className='flex items-center w-[98%] pb-10'>
+            <DatePicker
+                className="rounded mx-auto text-center"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="MM/yyyy"
+                showMonthYearPicker
+            />
+            <FaCalendarAlt className="ml-2" />
+        </div>
+        <div className='flex justify-end py-5 items-center w-[98%]'>
+            <button type="button" onClick={fetchData}>
+            Tampil Jadwal
+            </button>
+        </div>
+        <div className='flex flex-col items-center w-[98%] ml-[80px] pt-6'>
+            <div className="flex flex-col items-center w-[98%] bg-white px-5 py-3 shadow-md font-montserrat rounded-md">
+                <div className="w-full">
+                    <table className="table-auto w-full border-separate border-spacing-y-3">
                         <thead>
                             <tr>
-                            <th className="px-4 py-1 border-line border-b-2 text-line font-normal">No</th>
-                            <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Pimpinan Jemaah</th>
-                            <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Minggu ke-</th>
-                            <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Detail Waktu</th>
-                            <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Nama Mubaligh</th>
+                                <th className="px-4 py-1 border-line border-b-2 text-line font-normal">No</th>
+                                <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Pimpinan Jemaah</th>
+                                <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Minggu ke</th>
+                                <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Detail Hari</th>
+                                <th className="px-4 py-1 border-line border-b-2 text-line font-normal">Nama Mubaligh</th>
                             </tr>
                         </thead>
-                        <tbody className=''>
-                            <tr className='bg-[#F5F5F5] rounded-md shadow-md'>
-                            <td className="relative text-center px-4 py-2">1</td>
-                            <td className="relative text-center px-10 py-2 rounded-l-lg">Banjaran</td>
-                            <td className="relative text-center px-4 py-2">aa</td>
-                            <td className="relative text-center px-4 py-2">aa</td>
-                            <td className="relative text-center px-4 py-2 rounded-r-lg">aa</td>
-                            <div className='flex justify-center m-2'></div>
-                            </tr>
+                        <tbody>
+                        {data.map((v,i)=>{
+                                    return <tr className='bg-[#F5F5F5] rounded-md shadow-md' >
+                                    <td className="text-center w-36 px-4 py-2 rounded-l-lg">{i+1}</td>
+                                    <td className="text-center max-w-[25px] h-auto px-4 py-2">{v.PimpinanJemaah}</td>
+                                    <td className="text-center w-36 px-4 py-2 rounded-l-lg">{v.Minggu_ke}</td>
+                                    <td className="text-center w-36 px-4 py-2 rounded-l-lg">{v.hari}</td>
+                                    <td className="text-center w-36 px-4 py-2 rounded-l-lg">{v.Mubaligh}</td>
+                                    <td className=" relative items-center px-4 py-2 rounded-r-lg">
+                                    </td>
+                                </tr>
+                                })
+                            }
                         </tbody>
-                        </table>
-                    </div>
-                    </div>
-
+                    </table>
+                </div>
             </div>
+        </div>
+    </div>
         // </div>
     );
   };
