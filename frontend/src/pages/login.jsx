214cccdff00c4bf534784a8968cb31f942970990
aziztaufiqurrahman <taufiqqurrahman2899@gmail.com>
2024-06-28 15:45:35 +0700

    perbaikan

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -129,7 +129,7 @@ const Login = () => {
             <a href="https://profil.persisbanjaran.org" className="bg-black text-white font-bold text-xl p-4">SCHEDULEHUB</a>
           </div>
           <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
-            <p className="text-center text-3xl">Selamat Datang</p>
+            <p className="text-center text-3xl">Ahlan Wa Sahlan</p>
             <form className="flex flex-col pt-3 md:pt-8" onSubmit={handleLogin}>
               <div className="flex flex-col pt-4">
                 <label htmlFor="username" className="text-lg">Username</label>
@@ -138,7 +138,7 @@ const Login = () => {
                   id="username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
-                  placeholder="Masukkan NPA Anda"
+                  placeholder="Masukkan NIAT Anda"
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
                 />
               </div>
